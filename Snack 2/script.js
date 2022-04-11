f55b32b60fed47af27866dd38e3e3ae7bbc41b40
Ilthepriest <robertodelprete1993@libero.it>
2022-04-11 13:24:35 +0200

    add Snack 2

---
+++ Snack 2/script.js
@@ -0,0 +1,8 @@
+const arrayIniziale = ["grogu", "MANDO", "Luke"];
+
+console.log(arrayIniziale);
+
+ arrayIniziale.splice(0,1,"Grogu")
+ arrayIniziale.splice(1,1,"Mando")
+
+console.log(arrayIniziale);
